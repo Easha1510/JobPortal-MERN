2161303fc01ec64613ec5ff8f5292213a4b6241f
Easha <easrni.slp@gmail.com>
2026-01-29 10:12:59 +0530

    Initial commit - Job Portal MERN frontend

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div className='container 2xl:px-20 mx-auto my-10'>
+        <div>
+            <h2>Over 10,000+ jobs to apply</h2>
+            <p>Your Next Big Career Move Starts right Here - Explore the Best Job Opportunities and Take the First Step Towards Your Future!</p>
+            <div>
+                <div>
+                    <img src={assets.search_icon} alt="" />
+                    <input type="text" placeholder='Search for jobs' 
+                    className='max-sm:text-xs p-2 rounded outline-none w-full' />
+                </div>
+                <div>
+                    <img src={assets.location_icon} alt="" />
+                    <input type="text" placeholder='Location' 
+                    className='max-sm:text-xs p-2 rounded outline-none w-full' />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
