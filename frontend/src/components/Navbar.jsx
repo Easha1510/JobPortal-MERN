94970db462361b0857ed722252b758f877b76122
Easha <easrni.slp@gmail.com>
2026-01-31 09:43:42 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
+import {Link,useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -8,10 +9,12 @@ const Navbar = () => {
 
     const {user} = useUser()
 
+    const navigate = useNavigate() 
+
   return (
     <div className='shadow py-4'>
         <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-            <img src={assets.logo} alt="" />
+            <img onClick={()=> navigate('/')} className='cursor-pointer' src={assets.logo} alt="" />
             {
                 user 
                 ? <div className='flex items-center gap-3'>
