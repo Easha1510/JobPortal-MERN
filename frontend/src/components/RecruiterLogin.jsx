b9c7aaabc2ca428653625cf4372cff1b4c14fd43
Easha <easrni.slp@gmail.com>
2026-01-31 11:18:07 +0530

    updated

---
+++ frontend/src/components/RecruiterLogin.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const RecruiterLogin = () => {
+
+    const [state,setState]= useState('Login')
+    const [name,setName]= useState('')
+    const [password,setPassword] =useState('')
+    const [email,setEmail] = useState('')
+
+    const [image,setImage] =useState(false)
+    const [isTextDataSubmited,setIsTextDataSubmited]= useState(false)
+
+  return (
+    <div>
+        <form action="">
+            <h1>Recruiter {state}</h1>
+            <p>Welcome back</p>
+        </form>
+    </div>
+  )
+}
+
+export default RecruiterLogin
